d7ff1cb231c2bd0cb363d642f3d66fa1eb39abac
Raphael Gahona Massaro <raphael@massaro.rgm>
2023-04-22 21:12:03 -0300

    First Steps in Java with Springboot

---
+++ src/main/java/br/com/rgm/restwithspringbootandjava/RestWithSpringbootAndJavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.rgm.restwithspringbootandjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWithSpringbootAndJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWithSpringbootAndJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rgm/restwithspringbootandjava/RestWithSpringbootAndJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rgm.restwithspringbootandjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSpringbootAndJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
